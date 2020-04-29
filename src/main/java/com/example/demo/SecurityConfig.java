aad1b9366d060c5d2d3bb12fb762ad5e9bb59179
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-04-29 22:06:11 +0900

    implement login process(display error message)

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,15 +1,42 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 // セキュリティの設定用のクラス
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // SQLを実行できるようにする(BeanはSpringが用意している)
+    @Autowired
+    private DataSource dataSource;
+
+    // ユーザIDとパスワードを取得
+    private static final String USER_SQL = "SELECT"
+            + " user_id,"
+            + " password,"
+            + " true"
+            + " FROM"
+            + " m_user"
+            + " WHERE"
+            + " user_id = ?";
+
+    // ユーザのロールを取得
+    private static final String ROLE_SQL = "SELECT"
+            + " user_id,"
+            + " role"
+            + " FROM"
+            + " m_user"
+            + " WHERE"
+            + " user_id = ?";
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -30,8 +57,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login").permitAll() // ログインページは直リンクを許可
                 .anyRequest().authenticated(); // それ以外はログインしないと見れない
 
+        // ログイン処理
+        http
+                .formLogin()
+                .loginProcessingUrl("/login") // ログイン処理のパス(htmlのフォームの action="/login" の部分と一致させる
+                // 以下にメソッドチャーンで条件を追加する
+                .loginPage("/login") // ログインページの指定
+                .failureUrl("/login") // ログイン失敗時の遷移先
+                .usernameParameter("userId") // ログインページのユーザID
+                .passwordParameter("password") // ログインページのパスワード
+                .defaultSuccessUrl("/home", true); // ログイン成功後の遷移先
+
         // TODO: 有効にする
         // CSRF対策を無効(一時的)
         http.csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // ログイン処理時のユーザ情報をDBから取得する
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(USER_SQL)
+                .authoritiesByUsernameQuery(ROLE_SQL);
     }
 }
