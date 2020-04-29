381f64bce0b94662d39fcc5570d5e819cbf0b2ca
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-04-29 22:51:52 +0900

    implement logout process

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -78,6 +79,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .usernameParameter("userId") // ログインページのユーザID
                 .passwordParameter("password") // ログインページのパスワード
                 .defaultSuccessUrl("/home", true); // ログイン成功後の遷移先
+
+        // ログアウト処理
+        http
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // GETメソッドでリクエストをおくる
+                .logoutUrl("/logout") // POSTメソッドでログアウトする場合の設定
+                .logoutSuccessUrl("/login");
 
         // TODO: 有効にする
         // CSRF対策を無効(一時的)
