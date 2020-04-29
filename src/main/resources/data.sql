3119c8fbf74857a14667589a7d1dfd71ecd1d910
kiyota takeshi <kiyota@tagbangers.co.jp>
2020-04-29 21:22:09 +0900

    add security implementation scaffolding

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,6 @@ VALUES(1, '山田太郎', 30);
 
 INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
 VALUES('yamada@xxx.co.jp', 'password', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
+
+INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
+VALUES('tanaka@xxx.co.jp', 'password', '田中太郎', '1980-01-01', 40, false, 'ROLE_GENERAL');
